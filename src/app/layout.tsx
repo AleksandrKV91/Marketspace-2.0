caeb5cda18e6d8bc6808263212143295e4f9e319
AleksandrKV91 <avkashtanov91@gmail.com>
2026-04-03 14:02:41 +0300

    feat: update design tokens and glassmorphism CSS system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&family=Lato:wght@400;700&display=swap"
           rel="stylesheet"
         />
       </head>
